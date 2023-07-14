d25e85f65a4e81e4eba3d2b87975f742d933436c
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2023-07-14 13:09:44 +0000

    update script

--- Web Scraping/wiki_index.js
+++ Web Scraping/wiki_index.js
@@ -3,6 +3,8 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const path = require("path");
 let folder;
+let delay = 1500; // Initial delay in milliseconds between requests
+let retryAttempts = 0; // Number of retry attempts for a specific URL
 
 if (process.argv[2]) {
   folder = process.argv[2];
@@ -23,6 +25,10 @@ function downlaodFromFile() {
     startDownloading(data);
   });
 }
+
+function delayRequest(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
 function startDownloading(data) {
   const $ = cheerio.load(data);
   const imgElements = $("img[data-src]");
@@ -30,6 +36,8 @@ function startDownloading(data) {
     .map(async (index, element) => {
       const link = $(element).attr("data-src");
       const extratedLink = await extractUrl(link);
+      retryAttempts = 0; // Reset the retry attempts for each new URL
+      await delayRequest(delay);
       return extratedLink;
     })
     .get();
@@ -42,9 +50,45 @@ function startDownloading(data) {
 const extractUrl = async (url) => {
   const link = url.replace("/thumb", "").split("/").slice(0, 8).join("/");
   const destination = Math.random().toString(10).substring(7) + ".jpg";
+  retryAttempts = 0; // Reset the retry attempts for each new URL
   await downloadImage(link, `${folder}/${destination}`);
   return link;
 };
+
+// async function downloadImage(url, destination) {
+//   try {
+
+//      await delayRequest(1000);
+//     const response = await axios({
+//       method: "GET",
+//       url: url,
+//       timeout: 86400000,
+//       maxContentLength: Infinity,
+//       responseType: "arraybuffer",
+//     });
+
+//     if (response.status === 200) {
+//       const filePath = path.resolve(__dirname, destination);
+//       fs.writeFile(filePath, response.data, (err) => {
+//         if (err) {
+//           console.error("Error saving the image:", err);
+//         } else {
+//           console.log("Image downloaded successfully!");
+//         }
+//       });
+//     } else {
+
+//       console.error(
+//         "Failed to download the image. Status code:",
+//         response.status
+//       );
+//     }
+//   } catch (error) {
+//     console.log(url);
+//     console.error("Error downloading the image:", error.message);
+//   }
+// }
+
 async function downloadImage(url, destination) {
   try {
     const response = await axios({
@@ -54,7 +98,6 @@ async function downloadImage(url, destination) {
       maxContentLength: Infinity,
       responseType: "arraybuffer",
     });
-
     if (response.status === 200) {
       const filePath = path.resolve(__dirname, destination);
       fs.writeFile(filePath, response.data, (err) => {
@@ -64,6 +107,19 @@ async function downloadImage(url, destination) {
           console.log("Image downloaded successfully!");
         }
       });
+    } else if (response.status === 429) {
+      console.log("Retrying..");
+      if (retryAttempts < 3) {
+        // Implement exponential backoff by doubling the delay duration
+        delay *= 2;
+        retryAttempts++;
+        console.log(
+          `Received 429 status. Retrying in ${delay} milliseconds...`
+        );
+        await downloadImage(url, destination); // Retry the request
+      } else {
+        console.error("Exceeded maximum retry attempts for the URL:", url);
+      }
     } else {
       console.error(
         "Failed to download the image. Status code:",
@@ -72,5 +128,6 @@ async function downloadImage(url, destination) {
     }
   } catch (error) {
     console.error("Error downloading the image:", error.message);
+    await delayRequest(delay);
   }
 }
