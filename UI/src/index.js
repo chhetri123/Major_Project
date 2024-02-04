c358b83e085bdd45d6d76882cc4252c97c193517
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2024-02-04 16:35:57 +0000

    Setting Up File Uploading Interface

---
+++ UI/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./style.css";
+// Clear the existing HTML content
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
