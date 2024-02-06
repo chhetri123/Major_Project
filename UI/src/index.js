1ee20450546a5ceab4d97c9b38e2ef0f9d469a1d
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2024-02-06 17:48:52 +0000

    oraganizing folders

--- UI/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import "./style.css";
-// Clear the existing HTML content
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
