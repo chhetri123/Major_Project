ff300fb1879b7280d030d9962221a41d0ab53cc2
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2024-02-11 17:02:00 +0000

    update topbar

--- Project UI/src/Topbar.js
+++ Project UI/src/Topbar.js
@@ -1,23 +1,32 @@
-import "./topbar.css"
-import { useNavigate, Link } from 'react-router-dom';
+import "./topbar.css";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Topbar() {
-
   let navigate = useNavigate();
   const handleLogout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     navigate("/");
-  }
+  };
   return (
     <div className="topbarcontainer">
       {/* <div className="topbarleft"> */}
-        <div>
-          <span className="logo">Image Caption Generator</span>
-        </div>
-        <div>
-          {localStorage.getItem('token') && <button onClick={handleLogout} style={{marginRight:"42px"}} className="btnn">Logout</button>}
+      <div>
+        <span className="logo">
+          Image Caption Generation in Nepali Language
+        </span>
+      </div>
+      <div>
+        {localStorage.getItem("token") && (
+          <button
+            onClick={handleLogout}
+            style={{ marginRight: "42px" }}
+            className="btnn"
+          >
+            Logout
+          </button>
+        )}
         {/* </div> */}
       </div>
     </div>
-  )
+  );
 }
