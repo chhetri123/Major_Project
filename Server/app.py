90a0de89a6197eb6fa79378e64d6b4e7dafe2522
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2024-02-12 17:30:02 +0000

    updating model request

--- Server/app.py
+++ Server/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
 import cv2
-from keras.models import load_model
 import numpy as np
 from keras.applications import ResNet152
 from keras.optimizers import Adam
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model,load_model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers import add
 from keras.utils import to_categorical
+
+from tensorflow.keras.applications.resnet import preprocess_input
 from keras.preprocessing import image, sequence
 import cv2
 from keras_preprocessing.sequence import pad_sequences
@@ -14,8 +21,6 @@ import pickle
 import tensorflow as tf
 # from keras.applications.Resnet50 import preprocess_input
 from flask_cors import CORS
-
-from keras.applications import ResNet50
 # 
 # Transformer 
 from library.prediction import evaluate_single_image
@@ -38,7 +43,7 @@ loaded_transformer = Transformer(num_layer, d_model, num_heads, dff, row_size, c
                                  rate=dropout_rate)
 
 # Load the weights into the model
-loaded_transformer.load_weights('models/Transformer/model-80')
+loaded_transformer.load_weights('models/Transformer/model')
 # Use the loaded custom objects
 loaded_transformer.compile(optimizer=tf.keras.optimizers.Adam(learning_rate))
 print("Trasformer model loaded successfully")
@@ -54,21 +59,41 @@ print("Tokenizer  loaded successfully")
 
 # 
 
-
-incept_model = ResNet152(weights='imagenet', include_top=True)
-last = incept_model.layers[-2].output
-ResNet152Model= Model(inputs = incept_model.input,outputs = last)
-
-with open("pickle_files/lstm/words_dict.pkl","rb") as f:
+ResNet152Model=ResNet152(include_top=False, weights='imagenet',input_shape=(224,224,3), pooling='avg')
+with open("pickle_files/lstm/words_dict_nepali_sc.pkl","rb") as f:
     words_dict=pickle.load(f)
 
 
-vocab_size = len(words_dict)+1
-MAX_LEN = 192
+# vocab_size = len(words_dict)+1
+vocab_size = 5521
+# MAX_LEN = 192
+MAX_LEN=210
 inv_dict = {v:k for k, v in words_dict.items()}
 
 
-model = tf.keras.models.load_model('models/LSTM/lstm_model.h5')
+# model = tf.keras.models.load_model('models/LSTM/cultural_nepali_50.h5')
+
+inputs1 = Input(shape=(2048,))
+fe1 = Dropout(0.5)(inputs1)
+fe2 = Dense(256, activation='relu')(fe1)
+
+# language sequence model
+inputs2 = Input(shape=(MAX_LEN,))
+se1 = Embedding(vocab_size, MAX_LEN, mask_zero=True)(inputs2)
+se2 = Dropout(0.4)(se1)
+se3 = LSTM(256)(se2)
+
+# decoder model
+decoder1 = add([fe2, se3])
+decoder2 = Dense(256, activation='relu')(decoder1)
+outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+# tie it together [image, seq] [word]
+model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+model.load_weights("models/LSTM/resnet152_lstm_model_weights_50epoch.h5")
+#
 print("LSTM model  loaded successfully")
 
 
@@ -98,7 +123,7 @@ def tranformer():
     return jsonify({'caption': caption})
 
 
-@app.route('/after', methods=['POST'])
+@app.route('/lstm', methods=['POST'])
 def after():
 
     if 'file' not in request.files:
@@ -116,9 +141,13 @@ def after():
     # Read the saved file
     img = cv2.imread('static/file.jpg')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224,224))
-    img = img.reshape(1,224,224,3)
-    test_img_resized=ResNet152Model.predict(img).reshape(2048,)
+    # img = cv2.resize(img, (224,224))
+    img = cv2.resize(img, (224, 224))
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    img = preprocess_input(img) 
+    # img = img.reshape(1,224,224,3)
+    test_img_resized=ResNet152Model.predict(img).reshape(1,2048)
+    # test_img_resized=test_img_resized.reshape(test_img_resized.shape[0], -1)
 
     text_inp = ['startofseq']
     count = 0
